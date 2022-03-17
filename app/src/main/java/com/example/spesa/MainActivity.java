dbd9330f508b775d0540ee84458e9ae07f455b32
Quentin Benoist <benoist.quentin@yahoo.fr>
2022-03-17 12:11:44 +0100

    implementation des 2 premières pages

--- app/src/main/java/com/example/spesa/MainActivity.java
+++ app/src/main/java/com/example/spesa/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.spesa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchEditActivity(View view) {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
     }
 }
